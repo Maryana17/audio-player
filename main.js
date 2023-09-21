fd7be4983ef0b10a3ea0382ec387e73e57ab9ce6
Maryana17 <belrandevu@gmail.com>
2023-09-21 17:17:03 +0300

    fix: add function

---
+++ main.js
@@ -0,0 +1,13 @@
+const audio = document.querySelector('audio');
+const playBtn = document.querySelector('.play-btn');
+const pauseBtn = document.querySelector('.pause-btn');
+
+function playAudio() {
+  audio.currentTime = 0;
+  audio.play();
+}
+function pauseAudio() {
+  audio.pause();
+}
+playBtn.addEventListener('click', playAudio)
+pauseBtn.addEventListener('click', pauseAudio)

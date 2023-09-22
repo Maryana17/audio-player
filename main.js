2f61f8ca0b435a4b022f8531ade211396a1a9371
Maryana17 <belrandevu@gmail.com>
2023-09-22 20:43:05 +0300

    fix: add array

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const audio = document.querySelector('audio');
 const playBtn = document.querySelector('.play-btn');
 let isPlay = false;
+const playNum = 0;
 
 function playAudio() {
     if(!isPlay){
@@ -15,4 +16,11 @@ function playAudio() {
 }
 
 playBtn.addEventListener('click', playAudio)
+
+songsArray = [
+  {name: "Ed Sheeran", song: "Barcelona"},
+  {name: "Imagine Dragons", song: "Symphony"},
+  {name: "Sia", song: "Candy Cane Lane"},
+  {name: "Sia & David Guetta", song: "Floating Through Space"}
+]
 
